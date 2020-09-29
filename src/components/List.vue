ded9ca9972eeb4ba2ca8431812a520e5c5918f39
Dedal378 <orlovetsfoto@gmail.com>
2020-09-29 16:57:21 +0300

    5 add filter

--- src/components/List.vue
+++ src/components/List.vue
@@ -6,6 +6,58 @@
           justify="center">
         <v-col sm="10" md="8" lg="7">
           <h1 class="text--secondary mb-3">Users List</h1>
+          <v-autocomplete
+              v-model="model"
+              :items="usersList"
+              :loading="isLoading"
+              :search-input.sync="search"
+              chips
+              clearable
+              hide-details
+              hide-selected
+              item-text="name"
+              item-value="symbol"
+              label="Search..."
+              solo
+          >
+            <template v-slot:no-data>
+              <v-list-item>
+                <v-list-item-title>
+                  Search user for
+                  <strong>name</strong>
+                </v-list-item-title>
+              </v-list-item>
+            </template>
+            <template v-slot:selection="{ attr, on, item, selected }">
+              <v-chip
+                  v-bind="attr"
+                  :input-value="selected"
+                  color="blue-grey"
+                  class="white--text"
+                  v-on="on"
+              >
+                <v-icon left>
+                  mdi-bitcoin
+                </v-icon>
+                <span v-text="item.name"></span>
+              </v-chip>
+            </template>
+            <template v-slot:item="{ item }">
+              <v-list-item-avatar
+                  color="indigo"
+                  class="headline font-weight-light white--text"
+              >
+                {{ item.name.charAt(0) }}
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name"></v-list-item-title>
+                <v-list-item-subtitle v-text="item.symbol"></v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-icon>mdi-bitcoin</v-icon>
+              </v-list-item-action>
+            </template>
+          </v-autocomplete>
         </v-col>
       </v-row>
 
@@ -72,6 +124,38 @@
 import { mapGetters } from "vuex";
 
 export default {
+  data: () => ({
+    isLoading: false,
+    items: [],
+    model: null,
+    search: null,
+    tab: null,
+  }),
+
+  watch: {
+    model (val) {
+      if (val != null) this.tab = 0
+      else this.tab = null
+    },
+    // eslint-disable-next-line no-unused-vars
+    search (val) {
+      // Items have already been loaded
+      if (this.items.length > 0) return
+
+      this.isLoading = true
+
+      // Lazily load input items
+      fetch('https://api.coingecko.com/api/v3/coins/list')
+          .then(res => res.clone().json())
+          .then(res => {
+            this.items = res
+          })
+          .catch(err => {
+            console.log(err)
+          })
+          .finally(() => (this.isLoading = false))
+    },
+  },
   computed: {
     ...mapGetters(['usersList', 'loading']),
   },
